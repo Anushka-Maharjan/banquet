442e1efa0735626fb10d2866a77084643080ef2f
Prajesh.pradhan <prajeshpradhan2013@gmail.com>
2019-07-25 16:31:03 +0545

    Photographer Migration

---
+++ database/migrations/2019_07_18_231819_create_photographers_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePhotographersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('photographers', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('user_id');
+            $table->text('name');
+            $table->text('experience');
+            $table->text('address');
+            $table->text('genre');
+            $table->mediumText('banner');
+            $table->mediumText('dp');
+            $table->longText('bio');
+            $table->text('url')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('photographers');
+    }
+}
